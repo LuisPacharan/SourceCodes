7abeb200858ff9d8a2e79c63be7397bad99f06e1
luisenrique_pa <pacheco.arana.luis@gmail.com>
2022-07-06 13:07:49 -0500

    Create Conexion_SQL_JavaScript.js

---
+++ JAVASCRIPT/Conexion_SQL_JavaScript.js
@@ -0,0 +1,36 @@
+
+//LIBRERIAS//
+const sql = require("mssql");
+
+//********* MODULO DE CONEXION MICROSOFT SQL SERVER *********//
+const config = {
+
+    user: 'usuario',
+    password: 'password',
+    server: 'nombre\\servidor', // o DIRECCION IP
+    database: 'Nombre_base_detos', 
+    Options:{
+        trustedconnection:false,
+        enableArithAbort : true,
+        encrypt :true,
+        trustServerCertificate: true
+            }
+                };
+sql.on('error',err => {
+    console.log(err.message)
+})
+//////////////////////////////////////////////////////////////////
+
+async function getDB() {
+    try {
+        let pool = await sql.connect(config);
+        let result = await pool.request().query("select * from Customers");
+        console.log(result);
+        sql.close();
+    } catch(error){
+console.log(err.message);
+sql.close();
+    }
+}
+
+getDB();
