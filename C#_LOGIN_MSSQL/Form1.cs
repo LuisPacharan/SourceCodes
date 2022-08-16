9bcbcec419c9719e97c56a89e63c54766be1e859
Luis Enrique Pacheco Arana <85374478+LuisPacharan@users.noreply.github.com>
2022-08-16 11:02:30 -0500

    Add files via upload

---
+++ C#_LOGIN_MSSQL/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sistema_Web
+{
+    public partial class FormLogin : Form
+    {
+        public FormLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        SqlConnection mi_conexion = new SqlConnection("server = LAPTOP-BCVE4C0O\\PC_QUIQUE; database = DB_PRUEBAS; Integrated Security= true ");
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+
+            mi_conexion.Open();
+            SqlCommand ComandoSql = new SqlCommand("select usuario,contrasena from tbl_usuarios where usuario = @v_usr and CONVERT(varchar(50),DECRYPTBYPASSPHRASE('password',contrasena)) = @v_pass", mi_conexion);
+            ComandoSql.Parameters.AddWithValue("@v_usr", txtUsuario.Text);
+            ComandoSql.Parameters.AddWithValue("@v_pass", txtContrasena.Text);
+
+            SqlDataReader lector = ComandoSql.ExecuteReader();
+
+            if (lector.Read())
+            {
+                mi_conexion.Close();
+                Home pantalla = new Home();
+                pantalla.Show();
+            }
+
+        }
+    }
+}
