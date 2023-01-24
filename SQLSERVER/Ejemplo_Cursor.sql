a5410b779c0f0bc1dd8145e7ef0d8eb764348f6b
Luis Enrique Pacheco Arana <pacheco.arana.luis@gmail.com>
2023-01-23 21:46:25 -0600

    Ejemplo cursor

--- SQLSERVER/Ejemplo_Cursor.sql
+++ SQLSERVER/Ejemplo_Cursor.sql
@@ -1,8 +1,17 @@
 ---***Creacion de un Cursor***---
 
-DECLARE @Nombre varchar(400) -- Variable para guardar info
-DECLARE @Descripcion varchar(400) --Variable para guardar info
+DECLARE @Nombre VARCHAR(400) -- Variable para guardar info
+DECLARE @Telefono VARCHAR(400) --Variable para guardar info
 
-Declare InfoProd CURSOR FOR SELECT
+DECLARE InfoProd CURSOR DYNAMIC FOR SELECT Nombre, Telefono FROM TBL_SYS_WEB_CLIENTES --Declaracion de un Cursor
 
-Open InfoProd --Apertura del Cursor
+OPEN InfoProd --Apertura del Cursor
+FETCH NEXT FROM InfoProd INTO @Nombre , @Telefono
+WHILE @@fetch_status = 0
+BEGIN
+	PRINT CONCAT(@Nombre,'+'+ @Telefono, ' ' ,@@fetch_status)
+	FETCH NEXT FROM InfoProd INTO @Nombre , @Telefono --Recorrido del cursor y datos	
+END
+CLOSE  InfoProd		--Cierre del Cursor
+DEALLOCATE InfoProd --Desalojo del Cursor
+PRINT @@fetch_status
