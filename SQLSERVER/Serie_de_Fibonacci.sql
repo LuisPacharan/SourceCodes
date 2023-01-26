eb26a1958de2c48cd221d0ab2fc8908e1c9a48ce
Luis Enrique Pacheco Arana <pacheco.arana.luis@gmail.com>
2023-01-25 22:33:40 -0600

    Serie_Fibonacci

---
+++ SQLSERVER/Serie_de_Fibonacci.sql
@@ -0,0 +1,13 @@
+--Resuelve la Serie de Fibonacci que es la sucesión donde cada término es la suma de los dos anteriores.
+
+Print 'Fibonacci Series'
+declare @a int , @b int , @c int , @i int
+set @a=0 set @b=1 set @i=0 set @c=0 
+
+while @i<10
+	Begin
+		set @c=@a+@b
+		print @c
+		set @i=@i+1
+		set @a=@b set @b=@c
+	end
